34f0e798a3bb6f06649f03c66f66411c086659e9
sanath1599 <sanath15swaroop@gmail.com>
2020-02-03 13:14:11 +0530

    merged master

--- server.js
+++ server.js
@@ -214,10 +214,6 @@ app.listen(PORT, () => {
       "port"
     )} | Environment : ${app.get("env")}`
   );
-<<<<<<< HEAD
-  console.log("Returning true")
-  return true;
-=======
+  console.log("Returning true");
   return;
->>>>>>> fc43cddafddc5f4c5857ce6fb19c5940e7c6862c
 });
